7fe2fb0a6c27beeb8de6ca521893e8a76a3b9546
kikatkilla <mweb10228@gmail.com>
2022-11-09 11:41:25 -0800

    Sketch
    I sketched my world.

---
+++ RubysAdventurePerez/Assets/Scripts/RubyController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //creating the two variables that use the pre built axes
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Create the movement vector
+        Vector2 position = transform.position;
+
+        //changing the x and y position
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        
+        //Set the update position
+        transform.position = position;
+    }
+}
