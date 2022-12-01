08ef0e2fb1657d545d87408030b483d783322394
kikatkilla <mweb10228@gmail.com>
2022-12-01 11:44:04 -0800

    Rubys Health
    I gave Ruby her health today.

--- RubysAdventurePerez/Assets/Scripts/RubyController.cs
+++ RubysAdventurePerez/Assets/Scripts/RubyController.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    //Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
-
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        //creating the two variables that use the pre built axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        //Create the movement vector
-        Vector2 position = transform.position;
-
-        //changing the x and y position
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        
-        //Set the update position
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
+
+   public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
